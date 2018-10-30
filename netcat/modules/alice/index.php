6a3e038bad683337df2a9d199ddaa070a73e33cd
ftp <skddom_boris@batman.beget.ru>
2018-10-30 23:47:14 +0300

    automatic commit 2018_10_30_23-47

---
+++ netcat/modules/alice/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 31.10.2018
+ * Time: 1:46
+ */
+include_once '../../../vars.inc.php';
+include_once '../../connect_io.php';
+include_once '../default/function.inc.php';
