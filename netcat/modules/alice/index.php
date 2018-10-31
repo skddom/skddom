851069fb3ef99f130c64dad4e7d0585583cc7868
ftp <skddom_boris@batman.beget.ru>
2018-10-31 04:54:38 +0300

    automatic commit 2018_10_31_04-54

--- netcat/modules/alice/index.php
+++ netcat/modules/alice/index.php
@@ -8,3 +8,16 @@
 include_once '../../../vars.inc.php';
 include_once '../../connect_io.php';
 include_once '../default/function.inc.php';
+function _l( $s ) {
+	static $fr = FALSE;
+	$fn = __FILE__ . '.log';
+
+	file_put_contents( $fn, var_export( $s, 1 ). "\n" . str_repeat( '=', 40 ) . "\n", $fr ? FILE_APPEND : NULL );
+	$fr = TRUE;
+}
+
+$ret = array( 'ip' => $_SERVER['REMOTE_ADDR'] );
+_l( $_REQUEST );
+_l( $_SERVER );
+
+print json_encode( $ret );
